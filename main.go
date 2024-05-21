b2f6a475109c034352e768e3707409baef2dd750
Otavio Murilo <murilotavio10@gmail.com>
2024-05-21 00:30:14 -0300

    Deleting all other branches

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os/exec"
+	"strings"
 )
 
 func main() {
@@ -13,5 +14,24 @@ func main() {
 		fmt.Println(err.Error())
 	}
 
-	fmt.Println(string(stdout))
+	branches := strings.Split(string(stdout), "\n")
+	deleteBranches(branches)
+}
+
+func deleteBranches(branches []string) {
+	for _, branch := range branches {
+
+		if strings.Contains(branch, "* ") || branch == "" {
+			continue
+		}
+
+		cmd := exec.Command("git", "branch", "-d", strings.TrimSpace(branch))
+		stdout, err := cmd.Output()
+
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+
+		fmt.Print(string(stdout))
+	}
 }
